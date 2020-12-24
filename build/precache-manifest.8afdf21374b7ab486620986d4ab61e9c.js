3894c72d01f6f6df425eb2b1f5cacae82bec63cf
harimohan1990 <harimohan.info@gmail.com>
2020-12-24 19:30:47 +0530

    serverjs added

---
+++ build/precache-manifest.8afdf21374b7ab486620986d4ab61e9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262c03e801fbca6a60846b7f5e5ef42c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f049ea347cec926a1ca4",
+    "url": "/static/css/main.bcfe806a.chunk.css"
+  },
+  {
+    "revision": "45f44c1a17648a664b7b",
+    "url": "/static/js/2.7db1d980.chunk.js"
+  },
+  {
+    "revision": "f049ea347cec926a1ca4",
+    "url": "/static/js/main.e65d7301.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
